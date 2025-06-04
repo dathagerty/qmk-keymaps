3692f7da55c4ea7f8c0b4de2d14cef65bffcba1a
David Hagerty <david@dathagerty.com>
2025-06-03 20:40:48 -0400

    fix: switch space/enter for muscle memory

--- keyboards/thevankeyboards/minivan/keymaps/dathagerty/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/dathagerty/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     TAB_L1,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MO(_L1),
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(_L2),
-    KC_LCTL, MO(_L2), KC_LGUI,                   KC_ENT,  KC_SPC,                    KC_RALT, KC_ESC,  TG(_L3)
+    KC_LCTL, MO(_L2), KC_LGUI,                   KC_SPC,  KC_ENT,                    KC_RALT, KC_ESC,  TG(_L3)
   ),
   [_ST] = LAYOUT( /* STRNK */
     KC_ESC,  KC_F,    KC_D,    KC_L,    KC_B,    KC_V,    KC_J,    KC_G,    KC_O,    KC_U,    KC_COMM, KC_BSPC,
