d3bc1c023e42cc25f2fce0c461bf0a6c89ec2a5b
David Hagerty <david@dathagerty.com>
2025-06-03 20:25:26 -0400

    feat: add STRNK to minivan

--- keyboards/thevankeyboards/minivan/keymaps/dathagerty/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/dathagerty/keymap.c
@@ -7,7 +7,7 @@
 // entirely and just use numbers.
 
 #define _QW 0
-#define _DV 1
+#define _ST 1
 #define _CM 2
 #define _L1 3
 #define _L2 4
@@ -19,8 +19,8 @@
 #define R_CURBR LSFT(KC_RBRC)
 
 #define QWERTY PDF(_QW)
+#define STRNK PDF(_ST)
 #define COLEMAK PDF(_CM)
-#define DVORAK PDF(_DV)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
@@ -29,11 +29,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(_L2),
     KC_LCTL, MO(_L2), KC_LGUI,                   KC_ENT,  KC_SPC,                    KC_RALT, KC_ESC,  TG(_L3)
   ),
-  [_DV] = LAYOUT( /* Dvorak */
-    KC_TAB,  KC_SLSH, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
-    MO(_L1), KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    MO(_L1),
-    KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    MO(_L2),
-    KC_LCTL, MO(_L2), KC_LGUI,                   KC_ENT,  KC_SPC,                    KC_RALT, KC_ESC,  TG(_L3)
+  [_ST] = LAYOUT( /* STRNK */
+    KC_ESC,  KC_F,    KC_D,    KC_L,    KC_B,    KC_V,    KC_J,    KC_G,    KC_O,    KC_U,    KC_COMM, KC_BSPC,
+    MO(_L1), KC_S,    KC_T,    KC_R,    KC_N,    KC_K,    KC_Y,    KC_M,    KC_A,    KC_E,    KC_I,    MO(_L1),
+    KC_LSFT, KC_Z,    KC_Q,    KC_X,    KC_H,    KC_P,    KC_W,    KC_C,    KC_QUOT, KC_SCLN, KC_DOT,  MO(_L2),
+    KC_LCTL, MO(_L2), KC_LGUI,                   KC_SPC,  KC_ENT,                    KC_RALT, KC_ESC,  TG(_L3)
   ),
   [_CM] = LAYOUT( /* Colemak */
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_LGUI, _______,                   _______, _______,                   _______, _______, _______
   ),
   [_L2] = LAYOUT( /* LAYER 2 */
-    _______, QWERTY,  DVORAK,  COLEMAK, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_0,    _______,
+    _______, QWERTY,  STRNK,   COLEMAK, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_0,    _______,
     KC_ESC,  KC_PIPE, KC_DQUO, KC_UNDS, KC_PLUS, L_CURBR, R_CURBR, KC_4,    KC_5,    KC_6,    KC_VOLU, KC_ENT,
     _______, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_VOLD, _______,
     _______, _______, _______,                   _______, _______,                   _______, _______, _______
